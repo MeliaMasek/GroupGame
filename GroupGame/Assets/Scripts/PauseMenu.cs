c37b80d3457143a185ccb9002d06c319e2d8870b
Melia Masek <10910291@uvu.edu>
2024-01-10 10:43:38 -0700

    Added fonts and scripts

---
+++ GroupGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//code borrowed and modified by Hooson on youtube https://www.youtube.com/watch?v=tfzwyNS1LUY
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject shopMenu; 
+    public GameObject shuffleMenu;
+
+    private int sceneToContinue;
+    private int currentSceneIndex;
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void HomeOn()
+    {
+        mainMenu.SetActive(true);
+        Time.timeScale = 1f;
+    }
+   
+    public void HomeOff()
+    {
+        mainMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    
+    public void ShopOn()
+    {
+        shopMenu.SetActive(true);
+        Time.timeScale = 1f;
+    }
+    
+    public void ShopOff()
+    {
+        shopMenu.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    
+    public void ShuffleOn()
+    {
+        shuffleMenu.SetActive(true);
+        Time.timeScale = 1f;
+    }
+    
+    public void ShuffleOff()
+    {
+        shuffleMenu.SetActive(false);
+        Time.timeScale = 0f;
+    }
+}
