0edb618c807353859748c9b77bf18b5a759a54d4
Kaitlyn Graf <10957070@uvu.edu>
2024-01-19 23:37:18 -0700

    Added the start of click event and ingredients list scripts

---
+++ GroupGame/Assets/Scripts/Kaitlyn_Scripts/ClickBehavior.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClickBehavior : MonoBehaviour
+{
+    public UnityEvent onClickEvent;
+    private void OnMouseDown()
+    {
+        onClickEvent.Invoke();
+    }
+}
